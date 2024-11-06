b70cfdc49c82ac9ec407aec7617af4a08c0c3d2d
Filip Kin <96821611+filki@users.noreply.github.com>
2024-11-06 16:17:30 +0000

    another one

--- main.py
+++ main.py
@@ -1,65 +1,95 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
 def fetch_html(url):
     response = requests.get(url)
     response.raise_for_status()
     return response.text
 
-def extract_wikipedia_data(category_name):
-    base_url = "https://pl.wikipedia.org/wiki/Kategoria:"
-    category_url = base_url + category_name.replace(" ", "_")
+def get_category_articles(soup):
+    pages_div = soup.find("div", id="mw-pages")
+    if not pages_div:
+        print("No pages found in this category.")
+        return []
     
-    category_html = fetch_html(category_url)
-    category_soup = BeautifulSoup(category_html, 'html.parser')
-    
+    articles = [
+        {"url": "https://pl.wikipedia.org" + link["href"], "name": link["title"]}
+        for link in pages_div.find_all("a") if "title" in link.attrs
+    ]
+    return articles[:2]  
 
-    article_links = []
-    for link in category_soup.select("div.mw-category a")[:2]:  
-        article_links.append("https://pl.wikipedia.org" + link['href'])
-    
-    results = []
-    
-    for article_url in article_links:
-        article_html = fetch_html(article_url)
-        article_soup = BeautifulSoup(article_html, 'html.parser')
-        
-        internal_links = []
-        for link in article_soup.find_all("a", href=True, limit=5):
-            if link['href'].startswith('/wiki/') and not any(ns in link['href'] for ns in [':', '/File:', '/Kategorie:', '/Szablon:']):
-                internal_links.append(link.text)
-            if len(internal_links) >= 5:
-                break
-        
-        images = []
-        for img in article_soup.find_all("img", src=True, limit=3):
-            if "upload.wikimedia.org" in img['src']:
-                images.append("https:" + img['src'])
-        
-        external_links = []
-        for link in article_soup.find_all("a", href=True, limit=3):
-            if link['href'].startswith("http") and "wikipedia.org" not in link['href']:
-                external_links.append(link['href'])
-        
-        categories = []
-        for cat in article_soup.select("div#mw-normal-catlinks ul li a", limit=3):
-            categories.append(cat.text)
-        
-        article_result = []
-        
-        article_result.append(" | ".join(internal_links) if internal_links else "")
-        article_result.append(" | ".join(images) if images else "")
-        article_result.append(" | ".join(external_links) if external_links else "")
-        article_result.append(" | ".join(categories) if categories else "")
-        
-        results.append("\n".join(article_result))
-    
-    return results
-category_name = input().strip()
-data = extract_wikipedia_data(category_name)
+def extract_internal_titles(content_div):
+    titles = []
+    if content_div:
+        links = content_div.select('a:not(.extiw)')
+        titles = [
+            link.get('title') for link in links 
+            if link.get('title') and link.get_text(strip=True)
+        ][:5]
+    return titles
 
-for idx, article_data in enumerate(data, start=1):
-    print(article_data)
-    if idx < len(data):  
-        print()
+def extract_image_urls(content_div):
+    if not content_div:
+        return []
+    
+    images = content_div.find_all("img", src=True)
+    return [img["src"] for img in images[:3]]
+
+def extract_references(soup):
+    reference_urls = set()
+    
+    references_section = soup.find("ol", {"class": "references"})
+    if references_section:
+        links = references_section.find_all('a', class_='external text')
+        reference_urls.update(link.get('href') for link in links if link.get('href'))
+
+    citation_refs = soup.find_all("li", {"id": lambda x: x and x.startswith("cite")})
+    for ref in citation_refs:
+        link = ref.find('a', class_='external text')
+        if link and link.get('href'):
+            reference_urls.add(link.get('href'))
+    
+    return [url.replace("&", "&amp;") for url in list(reference_urls)[:3]]
+
+def extract_categories(soup):
+    categories_div = soup.find("div", {"id": "mw-normal-catlinks"})
+    if not categories_div:
+        return []
+    
+    category_links = categories_div.find_all("a")[1:4]  
+    return [link.get_text() for link in category_links]
+
+def process_article(article):
+    """Fetch and extract data from an individual article."""
+    article_html = fetch_html(article["url"])
+    article_soup = BeautifulSoup(article_html, "html.parser")
+
+    content_div = article_soup.find("div", {'id': 'mw-content-text', 'class': 'mw-body-content'})
+    titles = extract_internal_titles(content_div)
+    images = extract_image_urls(content_div)
+    references = extract_references(article_soup)
+    categories = extract_categories(article_soup)
+
+    print(" | ".join(titles))
+    print(" | ".join(images))
+    print(" | ".join(references))
+    print(" | ".join(categories))
+    print()  
+
+def search():
+    wiki_name = input("Enter Wikipedia category name: ")
+    url = "https://pl.wikipedia.org/wiki/Kategoria:" + wiki_name.replace(' ', '_')
+    
+    category_html = fetch_html(url)
+    soup = BeautifulSoup(category_html, "html.parser")
+    
+    articles = get_category_articles(soup)
+    if not articles:
+        return
+
+    for idx, article in enumerate(articles):
+        print(f"\nProcessing article {idx + 1}: {article['name']}")
+        process_article(article)
+
+if __name__ == "__main__":
+    search()
